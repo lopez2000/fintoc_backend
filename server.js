f6c92204c1b0acb685c58a5bba854987b39cc994
Pablo Baldazzi <pablo.baldazzi@uc.cl>
2024-03-15 16:24:00 -0300

    webhook secret added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
+const validateWebhookSignature = require('./webhookSignatureValidator'); // Import the signature validation function
 require('dotenv').config();
 
 const app = express();
@@ -9,12 +10,23 @@ const port = process.env.PORT || 8000;
 // Assuming the Fintoc secret API key is stored in an environment variable
 const FINTOC_API_KEY = process.env.FINTOC_API_KEY;
 const LINK_TOKEN = process.env.LINK_TOKEN;
+const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET; // Add this environment variable for the webhook secret
 
 app.use(bodyParser.json());
 
 // Define endpoint to handle webhook requests from Fintoc
 app.post('/', async (req, res) => {
   try {
+    // Validate webhook signature
+    const isValidSignature = validateWebhookSignature(req, WEBHOOK_SECRET);
+
+    if (!isValidSignature) {
+      console.log('Invalid webhook signature');
+      return res.status(403).send('Invalid webhook signature');
+    }
+
+    console.log('Valid webhook signature');
+
     console.log('Webhook received:', req.body);
 
     const webhookType = req.body.type;
@@ -37,12 +49,12 @@ app.post('/', async (req, res) => {
       console.log('Account refresh failed');
       res.send('Account refresh failed');
 
-    } else if (webhookType === account.refresh_intent.rejected) {
+    } else if (webhookType === 'account.refresh_intent.rejected') {
       console.log('Account webhook received');
       console.log('Account refresh rejected');
       res.send('Account refresh rejected');
 
-    } else if (webhookType === 'accont.refresh_intent.movements_removed') {
+    } else if (webhookType === 'account.refresh_intent.movements_removed') {
       console.log('Account webhook received');
       console.log('Movements removed');
       res.send('Movements removed');
